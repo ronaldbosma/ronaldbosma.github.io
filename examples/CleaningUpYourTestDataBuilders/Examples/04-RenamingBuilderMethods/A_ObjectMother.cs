f77a4c8e076f8a4e2a5714c02b0fc6a19f385a0c
ronaldbosma <ronaldbosma@outlook.com>
2019-03-11 19:23:42 +0000

    Refactor step to rename builder methods

---
+++ examples/CleaningUpYourTestDataBuilders/Examples/04-RenamingBuilderMethods/A_ObjectMother.cs
@@ -0,0 +1,9 @@
+namespace Examples._04_RenamingBuilderMethods
+{
+  class A
+  {
+    public static PersonBuilder Person => new PersonBuilder();
+
+    public static PersonBuilder Man => Person.IsMan();
+  }
+}
